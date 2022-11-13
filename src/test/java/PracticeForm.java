c9cd697d02207dddd25a08076a08e3657cc46053
KirillAndryushchenko <kirill2goo@gmail.com>
2022-11-13 13:18:04 +0300

    Удалены лишние пустые строки. Изменен путь к файлу с изображением.

--- src/test/java/PracticeForm.java
+++ src/test/java/PracticeForm.java
@@ -1,17 +1,12 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.selector.ByText;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
-
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class PracticeForm {
-
     // тестовые данные
     private String  firstName       = "Kirill",
                     lastName        = "Andryushchenko",
@@ -23,23 +18,21 @@ public class PracticeForm {
                     userBirthYear   = "2001",
                     subjects        = "Maths",
                     hobbies         = "Music",
-                    imagePath       = "src/test/images/profile.jpg",
+                    imagePath       = "profile.jpg",
                     currentAddress  = "Lenina Street",
                     userState       = "NCR",
                     userCity        = "Delhi";
 
-
     @BeforeAll
-    public static void beforeSettings(){
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+    public static void beforeSettings() {
+        Configuration.holdBrowserOpen   = true;
+        Configuration.browserSize       = "1920x1080";
+        Configuration.baseUrl           = "https://demoqa.com";
     }
-
 
     @Test
     @DisplayName("Отправка формы регистрации с валидными данными")
-    public void successfulSubmitOfRegistrationForm(){
+    public void successfulSubmitOfRegistrationForm() {
         open("/automation-practice-form");
 
         executeJavaScript("$('footer').remove()");
@@ -50,26 +43,20 @@ public class PracticeForm {
         $("#userEmail").setValue(userEmail);
         $("#genterWrapper").$(new ByText(userGender)).click();
         $("#userNumber").setValue(userPhone);
-
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-container").shouldBe(visible);
         $(".react-datepicker__month-select").selectOption(userBirthMonth);
         $(".react-datepicker__year-select").selectOption(userBirthYear);
         $x("//div[@class = 'react-datepicker__month']//div[text() = '" + userBirthDay + "']").click();
-
         $("#subjectsInput").setValue(subjects).pressEnter();
         $("#hobbiesWrapper").$(new ByText(hobbies)).click();
-
-        $("#uploadPicture").uploadFile(new File(imagePath));
-
+        $("#uploadPicture").uploadFromClasspath(imagePath);
         $("#currentAddress").setValue(currentAddress);
         $("#state").click();
         $("#stateCity-wrapper").$(new ByText(userState)).click();
         $("#city").click();
         $("#stateCity-wrapper").$(new ByText(userCity)).click();
-
         $("#submit").click();
-
         $(".modal-content").shouldBe(visible);
         $(".table-responsive").shouldHave(
                 text(firstName + " " + lastName),
@@ -82,6 +69,4 @@ public class PracticeForm {
                 text(userState + " " + userCity)
         );
     }
-
-
 }
